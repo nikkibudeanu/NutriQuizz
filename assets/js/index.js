db327e4d7a3c33e79aace7154a10b1f590e886d1
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-06 19:11:34 +0000

    add structure to the high scores page

---
+++ assets/js/index.js
@@ -0,0 +1,397 @@
+const question = document.getElementById('question');
+const options = Array.from(document.getElementsByClassName('options-content'));
+const questionNumberDisplay = document.getElementById('question-number');
+const scoreDisplay = document.getElementById('score');
+
+
+let currentQuestion={};
+let hasAnyOptionBeenClicked = false;
+let score= 0;
+let questionNumber =0;
+let availableQuestions =[];
+
+const QUESTION_BANK = {
+    1: [
+        {
+            question: "Which of the following nutrients provides energy to the body?",
+            option1: 'carbohydrates',
+            option2: 'vitamins',
+            option3: 'water',
+            option4: 'minerals',
+            answer: 1,
+        },
+        {
+            question: "Which nutrient supplies the most calories per gram? ",
+            option1: 'carbohydrates',
+            option2: 'fats',
+            option3: 'protein',
+            option4: 'water',
+            answer: 2,
+        },
+        {
+            question: "What is the main reason for eating a wide variety of foods? ",
+            option1: 'to learn the food label',
+            option2: 'to provide all the nutrients you need ',
+            option3: 'to keep from getting bored with your diet ',
+            option4: 'to help improve physical fitness',
+            answer: 2,
+        },
+        {
+            question: "The amount of energy the body gets from food is measured in.",
+            option1: 'Metabolism',
+            option2: 'Diet',
+            option3: 'Calories',
+            option4: 'Protein',
+            answer: 3,
+        },
+        {
+            question: "The three nutrients that provide the body with energy are:",
+            option1: 'carbohydrate, fat, and protein',
+            option2: 'calories, fat, and vitamins',
+            option3: 'carbohydrate, vitamins, and fat',
+            option4: 'calories, vitamins, and minerals',
+            answer: 1,
+        },
+        {
+            question: "Which of the following nutrients is important for body, cell, and muscle growth and repair?",
+            option1: 'vitamins',
+            option2: 'fats',
+            option3: 'proteins',
+            option4: 'carbohydrates',
+            answer: 3,
+        },
+        {
+            question: "_____ is/are the body's main or 1st choice of energy",
+            option1: 'water',
+            option2: 'carbohydrates',
+            option3: 'protein',
+            option4: 'fats',
+            answer: 2,
+        },
+        {
+            question: "Which food group does yogurt and milk belong in?",
+            option1: 'protein',
+            option2: 'vegetables',
+            option3: 'fruit',
+            option4: 'dairy',
+            answer: 4,
+        },
+        {
+            question: "____________ this substance is very addictive and is added to almost every processed food.",
+            option1: 'salt',
+            option2: 'sugar',
+            option3: 'msg',
+            option4: 'pepper',
+            answer: 2,
+        },
+        {
+            question: "______________ are very good for your immune system to be healthy and some can even fight cancer.",
+            option1: 'vegetables',
+            option2: 'meat',
+            option3: 'sugar',
+            option4: 'carbohydrates',
+            answer: 1,
+        },
+        {
+            question: "The number of calories that you can eat each day without gaining weight depends on your:",
+            option1: 'activity level',
+            option2: 'hunger',
+            option3: 'environment',
+            option4: 'mental health',
+            answer: 1,
+        },
+        {
+            question: "How many calories per gram does fat have?  ",
+            option1: '10',
+            option2: '9',
+            option3: '4',
+            option4: '7',
+            answer: 2,
+        }
+      ],
+      2: [ {
+        question: "Which of the following nutrients provides energy to the body?",
+        option1: 'carbohydrates',
+        option2: 'vitamins',
+        option3: 'water',
+        option4: 'minerals',
+        answer: 1,
+    },
+    {
+        question: "Which nutrient supplies the most calories per gram? ",
+        option1: 'carbohydrates',
+        option2: 'fats',
+        option3: 'protein',
+        option4: 'water',
+        answer: 2,
+    },
+    {
+        question: "What is the main reason for eating a wide variety of foods? ",
+        option1: 'to learn the food label',
+        option2: 'to provide all the nutrients you need ',
+        option3: 'to keep from getting bored with your diet ',
+        option4: 'to help improve physical fitness',
+        answer: 2,
+    },
+    {
+        question: "The amount of energy the body gets from food is measured in.",
+        option1: 'Metabolism',
+        option2: 'Diet',
+        option3: 'Calories',
+        option4: 'Protein',
+        answer: 3,
+    },
+    {
+        question: "The three nutrients that provide the body with energy are:",
+        option1: 'carbohydrate, fat, and protein',
+        option2: 'calories, fat, and vitamins',
+        option3: 'carbohydrate, vitamins, and fat',
+        option4: 'calories, vitamins, and minerals',
+        answer: 1,
+    },
+    {
+        question: "Which of the following nutrients is important for body, cell, and muscle growth and repair?",
+        option1: 'vitamins',
+        option2: 'fats',
+        option3: 'proteins',
+        option4: 'carbohydrates',
+        answer: 3,
+    },
+    {
+        question: "_____ is/are the body's main or 1st choice of energy",
+        option1: 'water',
+        option2: 'carbohydrates',
+        option3: 'protein',
+        option4: 'fats',
+        answer: 2,
+    },
+    {
+        question: "Which food group does yogurt and milk belong in?",
+        option1: 'protein',
+        option2: 'vegetables',
+        option3: 'fruit',
+        option4: 'dairy',
+        answer: 4,
+    },
+    {
+        question: "____________ this substance is very addictive and is added to almost every processed food.",
+        option1: 'salt',
+        option2: 'sugar',
+        option3: 'msg',
+        option4: 'pepper',
+        answer: 2,
+    },
+    {
+        question: "______________ are very good for your immune system to be healthy and some can even fight cancer.",
+        option1: 'vegetables',
+        option2: 'meat',
+        option3: 'sugar',
+        option4: 'carbohydrates',
+        answer: 1,
+    },
+    {
+        question: "The number of calories that you can eat each day without gaining weight depends on your:",
+        option1: 'activity level',
+        option2: 'hunger',
+        option3: 'environment',
+        option4: 'mental health',
+        answer: 1,
+    },
+    {
+        question: "How many calories per gram does fat have?  ",
+        option1: '10',
+        option2: '9',
+        option3: '4',
+        option4: '7',
+        answer: 2,
+    }],
+      3: [ {
+        question: "Which of the following nutrients provides energy to the body?",
+        option1: 'carbohydrates',
+        option2: 'vitamins',
+        option3: 'water',
+        option4: 'minerals',
+        answer: 1,
+    },
+    {
+        question: "Which nutrient supplies the most calories per gram? ",
+        option1: 'carbohydrates',
+        option2: 'fats',
+        option3: 'protein',
+        option4: 'water',
+        answer: 2,
+    },
+    {
+        question: "What is the main reason for eating a wide variety of foods? ",
+        option1: 'to learn the food label',
+        option2: 'to provide all the nutrients you need ',
+        option3: 'to keep from getting bored with your diet ',
+        option4: 'to help improve physical fitness',
+        answer: 2,
+    },
+    {
+        question: "The amount of energy the body gets from food is measured in.",
+        option1: 'Metabolism',
+        option2: 'Diet',
+        option3: 'Calories',
+        option4: 'Protein',
+        answer: 3,
+    },
+    {
+        question: "The three nutrients that provide the body with energy are:",
+        option1: 'carbohydrate, fat, and protein',
+        option2: 'calories, fat, and vitamins',
+        option3: 'carbohydrate, vitamins, and fat',
+        option4: 'calories, vitamins, and minerals',
+        answer: 1,
+    },
+    {
+        question: "Which of the following nutrients is important for body, cell, and muscle growth and repair?",
+        option1: 'vitamins',
+        option2: 'fats',
+        option3: 'proteins',
+        option4: 'carbohydrates',
+        answer: 3,
+    },
+    {
+        question: "_____ is/are the body's main or 1st choice of energy",
+        option1: 'water',
+        option2: 'carbohydrates',
+        option3: 'protein',
+        option4: 'fats',
+        answer: 2,
+    },
+    {
+        question: "Which food group does yogurt and milk belong in?",
+        option1: 'protein',
+        option2: 'vegetables',
+        option3: 'fruit',
+        option4: 'dairy',
+        answer: 4,
+    },
+    {
+        question: "____________ this substance is very addictive and is added to almost every processed food.",
+        option1: 'salt',
+        option2: 'sugar',
+        option3: 'msg',
+        option4: 'pepper',
+        answer: 2,
+    },
+    {
+        question: "______________ are very good for your immune system to be healthy and some can even fight cancer.",
+        option1: 'vegetables',
+        option2: 'meat',
+        option3: 'sugar',
+        option4: 'carbohydrates',
+        answer: 1,
+    },
+    {
+        question: "The number of calories that you can eat each day without gaining weight depends on your:",
+        option1: 'activity level',
+        option2: 'hunger',
+        option3: 'environment',
+        option4: 'mental health',
+        answer: 1,
+    },
+    {
+        question: "How many calories per gram does fat have?  ",
+        option1: '10',
+        option2: '9',
+        option3: '4',
+        option4: '7',
+        answer: 2,
+    }]
+  };
+
+  
+  
+//CONSTANTS
+  const CORRECT_AWARD =1 ;
+  const MAX_QUESTIONS =12;
+  
+  function getQuizLevel() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const level = urlParams.get('level');
+    return level;
+  }
+  
+  function startGame() {
+    questionNumber=0;
+    score=0;
+
+    const level = getQuizLevel();
+    availableQuestions=[...QUESTION_BANK[1]];
+
+
+    // Randomise the question sequence
+    availableQuestions = availableQuestions.sort( () => .5 - Math.random() );
+    addEventListenerToOptions();
+    getNextQuestion();
+}
+
+function getNextQuestion() {
+  
+    // End of quiz 
+    if(questionNumber >= MAX_QUESTIONS){
+        localStorage.setItem('recentScore', score);
+        return window.location.assign("/final-page1.html");
+    }
+
+    questionNumber++;
+// Update question number text on UI
+    questionNumberDisplay.innerText = questionNumber + "/" + MAX_QUESTIONS;
+
+// Picking the next question index which comes sequentially
+currentQuestion = availableQuestions[questionNumber - 1];
+question.innerText = currentQuestion.question;
+
+
+// Displaying each option
+options.forEach(option => {
+    const number = option.dataset['number'];
+    option.innerText = currentQuestion['option' + number];
+});
+    
+    hasAnyOptionBeenClicked=false;
+}
+
+
+function addEventListenerToOptions() {
+    options.forEach(option=>{
+      option.addEventListener('click', e => {
+          if(hasAnyOptionBeenClicked) return;
+  
+          hasAnyOptionBeenClicked = true;
+      
+          const clickedOption = e.target;
+          const clickedAnswer = clickedOption.dataset['number'];
+  
+          const isAnswerCorrect = clickedAnswer == currentQuestion.answer;
+          
+          const classToApply = isAnswerCorrect ? "correct" : "incorrect";
+          
+  
+          if(isAnswerCorrect) {
+              raiseScore();
+          }
+  
+          clickedOption.parentElement.classList.add(classToApply);
+  
+          const timeoutRef = setTimeout(() => {
+              clickedOption.parentElement.classList.remove(classToApply);
+              getNextQuestion();
+              clearTimeout(timeoutRef);
+            }, 1000);
+      });
+    });
+  }
+    
+  function raiseScore() {
+      score += CORRECT_AWARD;
+      scoreDisplay.innerText = score;
+  }
+  
+  startGame();
+        
+  
+      
