a68ab0726b4389227c97688cb4089ac6ddf2cb6b
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-07 19:10:28 +0000

    add changes to style and pattern element to input field

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// button variable
+var button = document.getElementById("back-to-top-btn");
+
+
+// When the user clicks on the button, scroll to the top of the document
+function backToTopFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
