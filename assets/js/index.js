3b13b34cb74f54178b4f67f8e252f391161c427b
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-07 12:55:56 +0000

    add changes to the javascript file as the project structure changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,8 +10,7 @@ let score= 0;
 let questionNumber =0;
 let availableQuestions =[];
 
-const QUESTION_BANK = {
-    1: [
+const QUESTION_BANK = [
         {
             question: "Which of the following nutrients provides energy to the body?",
             option1: 'carbohydrates',
@@ -108,200 +107,8 @@ const QUESTION_BANK = {
             option4: '7',
             answer: 2,
         }
-      ],
-      2: [ {
-        question: "Which of the following nutrients provides energy to the body?",
-        option1: 'carbohydrates',
-        option2: 'vitamins',
-        option3: 'water',
-        option4: 'minerals',
-        answer: 1,
-    },
-    {
-        question: "Which nutrient supplies the most calories per gram? ",
-        option1: 'carbohydrates',
-        option2: 'fats',
-        option3: 'protein',
-        option4: 'water',
-        answer: 2,
-    },
-    {
-        question: "What is the main reason for eating a wide variety of foods? ",
-        option1: 'to learn the food label',
-        option2: 'to provide all the nutrients you need ',
-        option3: 'to keep from getting bored with your diet ',
-        option4: 'to help improve physical fitness',
-        answer: 2,
-    },
-    {
-        question: "The amount of energy the body gets from food is measured in.",
-        option1: 'Metabolism',
-        option2: 'Diet',
-        option3: 'Calories',
-        option4: 'Protein',
-        answer: 3,
-    },
-    {
-        question: "The three nutrients that provide the body with energy are:",
-        option1: 'carbohydrate, fat, and protein',
-        option2: 'calories, fat, and vitamins',
-        option3: 'carbohydrate, vitamins, and fat',
-        option4: 'calories, vitamins, and minerals',
-        answer: 1,
-    },
-    {
-        question: "Which of the following nutrients is important for body, cell, and muscle growth and repair?",
-        option1: 'vitamins',
-        option2: 'fats',
-        option3: 'proteins',
-        option4: 'carbohydrates',
-        answer: 3,
-    },
-    {
-        question: "_____ is/are the body's main or 1st choice of energy",
-        option1: 'water',
-        option2: 'carbohydrates',
-        option3: 'protein',
-        option4: 'fats',
-        answer: 2,
-    },
-    {
-        question: "Which food group does yogurt and milk belong in?",
-        option1: 'protein',
-        option2: 'vegetables',
-        option3: 'fruit',
-        option4: 'dairy',
-        answer: 4,
-    },
-    {
-        question: "____________ this substance is very addictive and is added to almost every processed food.",
-        option1: 'salt',
-        option2: 'sugar',
-        option3: 'msg',
-        option4: 'pepper',
-        answer: 2,
-    },
-    {
-        question: "______________ are very good for your immune system to be healthy and some can even fight cancer.",
-        option1: 'vegetables',
-        option2: 'meat',
-        option3: 'sugar',
-        option4: 'carbohydrates',
-        answer: 1,
-    },
-    {
-        question: "The number of calories that you can eat each day without gaining weight depends on your:",
-        option1: 'activity level',
-        option2: 'hunger',
-        option3: 'environment',
-        option4: 'mental health',
-        answer: 1,
-    },
-    {
-        question: "How many calories per gram does fat have?  ",
-        option1: '10',
-        option2: '9',
-        option3: '4',
-        option4: '7',
-        answer: 2,
-    }],
-      3: [ {
-        question: "Which of the following nutrients provides energy to the body?",
-        option1: 'carbohydrates',
-        option2: 'vitamins',
-        option3: 'water',
-        option4: 'minerals',
-        answer: 1,
-    },
-    {
-        question: "Which nutrient supplies the most calories per gram? ",
-        option1: 'carbohydrates',
-        option2: 'fats',
-        option3: 'protein',
-        option4: 'water',
-        answer: 2,
-    },
-    {
-        question: "What is the main reason for eating a wide variety of foods? ",
-        option1: 'to learn the food label',
-        option2: 'to provide all the nutrients you need ',
-        option3: 'to keep from getting bored with your diet ',
-        option4: 'to help improve physical fitness',
-        answer: 2,
-    },
-    {
-        question: "The amount of energy the body gets from food is measured in.",
-        option1: 'Metabolism',
-        option2: 'Diet',
-        option3: 'Calories',
-        option4: 'Protein',
-        answer: 3,
-    },
-    {
-        question: "The three nutrients that provide the body with energy are:",
-        option1: 'carbohydrate, fat, and protein',
-        option2: 'calories, fat, and vitamins',
-        option3: 'carbohydrate, vitamins, and fat',
-        option4: 'calories, vitamins, and minerals',
-        answer: 1,
-    },
-    {
-        question: "Which of the following nutrients is important for body, cell, and muscle growth and repair?",
-        option1: 'vitamins',
-        option2: 'fats',
-        option3: 'proteins',
-        option4: 'carbohydrates',
-        answer: 3,
-    },
-    {
-        question: "_____ is/are the body's main or 1st choice of energy",
-        option1: 'water',
-        option2: 'carbohydrates',
-        option3: 'protein',
-        option4: 'fats',
-        answer: 2,
-    },
-    {
-        question: "Which food group does yogurt and milk belong in?",
-        option1: 'protein',
-        option2: 'vegetables',
-        option3: 'fruit',
-        option4: 'dairy',
-        answer: 4,
-    },
-    {
-        question: "____________ this substance is very addictive and is added to almost every processed food.",
-        option1: 'salt',
-        option2: 'sugar',
-        option3: 'msg',
-        option4: 'pepper',
-        answer: 2,
-    },
-    {
-        question: "______________ are very good for your immune system to be healthy and some can even fight cancer.",
-        option1: 'vegetables',
-        option2: 'meat',
-        option3: 'sugar',
-        option4: 'carbohydrates',
-        answer: 1,
-    },
-    {
-        question: "The number of calories that you can eat each day without gaining weight depends on your:",
-        option1: 'activity level',
-        option2: 'hunger',
-        option3: 'environment',
-        option4: 'mental health',
-        answer: 1,
-    },
-    {
-        question: "How many calories per gram does fat have?  ",
-        option1: '10',
-        option2: '9',
-        option3: '4',
-        option4: '7',
-        answer: 2,
-    }]
-  };
+        ];
+
 
   
   
@@ -320,7 +127,7 @@ const QUESTION_BANK = {
     score=0;
 
     const level = getQuizLevel();
-    availableQuestions=[...QUESTION_BANK[1]];
+    availableQuestions=[...QUESTION_BANK];
 
 
     // Randomise the question sequence
