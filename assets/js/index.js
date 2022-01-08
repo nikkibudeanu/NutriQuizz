89ce99e4cc9b5956d8f8de7007d790d794110d04
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-08 09:52:05 +0000

    add javascript code for animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,4 +8,46 @@ function backToTopFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+// scroll animation
 
+//scroll variables
+const scrollElements = document.querySelectorAll(".jsScroll");
+
+const inView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const outofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const display = (element) => {
+  element.classList.add("done");
+};
+
+const hide = (element) => {
+  element.classList.remove("done");
+};
+
+const scrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (inView(el, 1.25)) {
+      display(el);
+    } else if (outofView(el)) {
+      hide(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  scrollAnimation();
+});
