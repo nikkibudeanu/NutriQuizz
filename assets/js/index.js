fecce2c009cb5e96db1526c2bd2dd75cf32a966b
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-09 11:41:28 +0000

    add code comments to the index js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,9 +10,10 @@ function backToTopFunction() {
 
 // scroll animation
 
-//scroll variables inspired by https://webdesign.tutsplus.com/tutorials/animate-on-scroll-with-javascript--cms-36671
+//scroll variables inspired by --> https://webdesign.tutsplus.com/tutorials/animate-on-scroll-with-javascript--cms-36671
 const scrollElements = document.querySelectorAll(".jsScroll");
 
+// used element.getBoundingClientRect method to get the information about sizing and positions --> inspired by --> https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
 const inView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top;
 
@@ -30,6 +31,7 @@ const outofView = (el) => {
   );
 };
 
+// create functions to start/stop animation when scrolling is done +change opacity from 0 to 1 for the scrolled elements
 const display = (element) => {
   element.classList.add("done");
 };
@@ -38,6 +40,7 @@ const hide = (element) => {
   element.classList.remove("done");
 };
 
+// create function for scroll animation
 const scrollAnimation = () => {
   scrollElements.forEach((el) => {
     if (inView(el, 1.25)) {
@@ -47,7 +50,7 @@ const scrollAnimation = () => {
     }
   })
 }
-
+//add event listener for scrolling
 window.addEventListener("scroll", () => { 
   scrollAnimation();
 });
