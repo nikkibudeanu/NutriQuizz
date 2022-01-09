ec42964ad591d424661323769c3f411425b93743
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-09 11:50:04 +0000

    add comments to the rest of the js files

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,5 +1,7 @@
 // learned how to do this on. --> https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
+// the function below is an onclick function
 function hamburgerMenu() {
+  // ad variable for navigation bar links
     var x = document.getElementById("links");
     if (x.style.display === "block") {
       x.style.display = "none";
--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,9 +1,17 @@
+
+// variables for the high score list 
 const highestScoresList = document.getElementById("highestScoresList");
+
+// use JSON parse methods to get high score data from the local storage  and create an array from it
+
 const highestScores = JSON.parse(localStorage.getItem("highestScores")) || [];
 
+// create a list to display the high scores
 highestScoresList.innerHTML = highestScores
+//create a new array using map()
   .map(score => {
     return `<li class="high-score">${score.name} : ${score.score}</li>`;
     
   })
+  // create an return the array with high scores. Learnt from --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
   .join("");
