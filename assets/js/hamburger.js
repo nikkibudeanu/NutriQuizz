f08afcd98b9c70a88d34830ecac5fc017cd3eb4d
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-01-09 11:44:19 +0000

    add explanation to code into the hamburger menu js file

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,3 +1,4 @@
+// learned how to do this on. --> https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
 function hamburgerMenu() {
     var x = document.getElementById("links");
     if (x.style.display === "block") {
